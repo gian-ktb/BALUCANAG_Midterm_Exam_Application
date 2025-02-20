bec1bb0869b37d621bf349dc347987a9f06b41f4
Beefsteak <153777616+gian-ktb@users.noreply.github.com>
2025-02-20 09:51:38 +0000

    Added more movies (mainly all the movies of Christopher Nolan)

--- BALUCANAG_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
+++ BALUCANAG_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -12,6 +12,13 @@ class ProductController extends Controller
             ["title" => "Inception", "genre" => "Sci-Fi", "rating" => 8.8],
             ["title" => "The Dark Knight", "genre" => "Action", "rating" => 9.0],
             ["title" => "Interstellar", "genre" => "Sci-Fi", "rating" => 8.6],
+            ["title" => "Tenet", "genre" => "Sci-Fi", "rating" => 7.3],
+            ["title" => "Dunkirk", "genre" => "War", "rating" => 7.9],
+            ["title" => "The Prestige", "genre" => "Drama", "rating" => 8.5],
+            ["title" => "Batman Begins", "genre" => "Action", "rating" => 8.2],
+            ["title" => "Oppenheimer", "genre" => "Biography", "rating" => 8.4],
+            ["title" => "Memento", "genre" => "Thriller", "rating" => 8.4],
+            ["title" => "Insomnia", "genre" => "Thriller", "rating" => 7.2]
         ];
 
         return view('products.index', compact('products'));
